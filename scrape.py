0b621c8788463677b54d2dd15429c4995a141998
Evan Bryer <evanbryer@gmail.com>
2021-08-24 13:12:05 -0400

    Prevented threads from appearing multiple times

--- scrape.py
+++ scrape.py
@@ -5,14 +5,18 @@ pre = 'https://www.lainchan.org/Ω'
 html = requests.get('https://www.lainchan.org/Ω/catalog.html')
 soup = BeautifulSoup(html.text, 'html.parser')
 links = []
+cache = []
 for thread in soup.find_all('div', class_='thread'):
     for ref in thread.find_all('a'):
         l = ref.get('href')
         if '/Î©/res/' in l:
             links.append(l.split('Î©')[1])
-            
+
 for link in links:
     html = requests.get(pre + link)
     soup = BeautifulSoup(html.text, 'html.parser')
     thread = soup.find_all('div', class_='body')[0]
-    print(thread.get_text('\n\n'), '\n\n')
+    text = thread.get_text('\n\n')
+    if text not in cache:
+        cache.append(text)
+        print(text, '\n\n')
